51bbb5faf8c1f415fcc7e300adb41e5e826e39da
joergboe <joergboe@de.ibm.com>
2019-12-17 15:42:15 +0100

    New IAMAccessTokenGenerator

--- tests/frameworktests/tests/TestProperties.sh
+++ tests/frameworktests/tests/TestProperties.sh
@@ -1,5 +1,10 @@
+#enhance timeout due to log running compile jobs for samples
+setVar TTPR_timeout 300
+
 #samples path
 setVar 'TTPR_SreamsxSttgatewaySamplesPath' "$TTRO_inputDir/../../../samples"
 
 #toolkit path
 setVar 'TTPR_streamsxSttgatewayToolkit' "$TTRO_inputDir/../../../com.ibm.streamsx.sttgateway"
+
+#do not set TT_toolkitPath here because it is assembled in global TestSuite.sh
